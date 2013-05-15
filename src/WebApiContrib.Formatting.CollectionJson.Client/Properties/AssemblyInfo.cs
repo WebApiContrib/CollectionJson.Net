07ae18bfd2c615b2a4b891965f6f36b6af46be59
Glenn Block <glenn.block@gmail.com>
2013-05-14 22:13:13 -0700

    Adding shared version file

--- src/WebApiContrib.Formatting.CollectionJson.Client/Properties/AssemblyInfo.cs
+++ src/WebApiContrib.Formatting.CollectionJson.Client/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7ba47b4a-d501-46d1-9aff-9944a01712cf")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- src/WebApiContrib.Formatting.CollectionJson.Server/Properties/AssemblyInfo.cs
+++ src/WebApiContrib.Formatting.CollectionJson.Server/Properties/AssemblyInfo.cs
@@ -22,15 +22,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d59672cd-737a-4c2e-951a-d15ab8b4f672")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.2.0")]
-[assembly: AssemblyFileVersion("0.0.2.0")]
