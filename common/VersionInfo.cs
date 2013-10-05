acaac40040b514af5e5d36acd5265d5fc1e00c7d
Glenn Block <glenn.block@gmail.com>
2013-10-04 17:14:58 -0700

    Adding all nuspecs to the solution and updating versions to 0.1.2.0

--- common/VersionInfo.cs
+++ common/VersionInfo.cs
@@ -11,5 +11,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
