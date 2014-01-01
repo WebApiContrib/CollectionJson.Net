a79f9f1c54a2fc3ddff4924eb4c73f3fa402298b
Glenn Block <glenn.block@gmail.com>
2014-01-01 02:06:58 +0000

    Updating nuspec version to use $ and fixing assembly copyright

--- src/WebApiContrib.Formatting.CollectionJson.Server/Properties/AssemblyInfo.cs
+++ src/WebApiContrib.Formatting.CollectionJson.Server/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WebApiContrib.Formatting.CollectionJson")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright 2013 Glenn Block")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
