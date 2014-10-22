b7f74fbf80f9e83ef7910c89732efbc4b8ca08f7
Glenn Block <glenn.block@gmail.com>
2014-10-22 16:55:50 -0700

    Adding abstract writer class with new overload

---
+++ src/CollectionJson/CollectionJsonDocumentWriter_Of_T.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CollectionJson
+{
+    public abstract class CollectionJsonDocumentWriter<TItem> : ICollectionJsonDocumentWriter<TItem>
+    {
+        public virtual IReadDocument Write(IEnumerable<TItem> data, Uri uri)
+        {
+            return null;
+        }
+
+        public virtual IReadDocument Write(IEnumerable<TItem> data)
+        {
+            return null;
+        }
+    }
+}
