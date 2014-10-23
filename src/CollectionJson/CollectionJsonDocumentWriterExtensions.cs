18042e01bf459d9ff10171dfafbcfa4ed787114c
Jonathan Channon <jonathan.channon@gmail.com>
2014-10-23 11:21:20 +0100

    added extension for abstract writer

--- src/CollectionJson/CollectionJsonDocumentWriterExtensions.cs
+++ src/CollectionJson/CollectionJsonDocumentWriterExtensions.cs
@@ -4,7 +4,12 @@
     {
         public static IReadDocument Write<TItem>(this ICollectionJsonDocumentWriter<TItem> writer, TItem item)
         {
-            return writer.Write(new[] {item});
+            return writer.Write(new[] { item });
+        }
+
+        public static IReadDocument Write<TItem>(this CollectionJsonDocumentWriter<TItem> writer, TItem item, Uri uri)
+        {
+            return writer.Write(new[] { item }, uri);
         }
     }
 }
