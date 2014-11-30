310ae58e3d9f33bf19c02a065e356044af6ae0c9
Glenn Block <glenn.block@gmail.com>
2014-11-29 18:23:02 -0800

    Adds support for Reading to an IWriteDocument and tests. Fix for #105. Also did some other cleanup

--- src/CollectionJson/Collection.cs
+++ src/CollectionJson/Collection.cs
@@ -10,6 +10,8 @@ namespace CollectionJson
     [DataContract]
     public class Collection : ExtensibleObject
     {
+        public const string MediaType = "application/vnd.collection+json";
+
         public Collection()
         {
             Links = new List<Link>();
